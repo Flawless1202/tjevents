95ed23e4436ac1d37219b8d1547cb1d966d1293c
Kai Chen <14sdck@tongji.edu.cn>
2020-08-02 01:23:50 +0800

    - add event2graph dataset support

---
+++ tjevents/datasets/event2graph.py
@@ -0,0 +1,54 @@
+import os
+
+import numpy as np
+from scipy import io
+
+import torch
+from torch_geometric.data import Dataset, Data
+from torch_geometric.nn import radius_graph
+
+from tjevents.datasets import GridSampling
+
+
+class E2GDataset(Dataset):
+
+    def __init__(self, root, transform=None, pre_transform=None):
+        super(E2GDataset, self).__init__(root, transform, pre_transform)
+
+    @property
+    def raw_file_names(self):
+        file_names = os.listdir(self.raw_dir)
+        return file_names
+
+    @property
+    def processed_file_names(self):
+        file_names = [f.replace(".mat", ".pt") for f in os.listdir(self.raw_dir)]
+        return file_names
+
+    def download(self):
+        return
+
+    def process(self):
+        for raw_path in self.raw_paths:
+            events = io.loadmat(raw_path)
+            events = np.concatenate((events["ts"] / 1000.0, events["x"],
+                                     events["y"], events["pol"]), axis=1).astype(np.float32)
+            events = torch.from_numpy(events)
+            label = ord(raw_path.split("/")[-1][0]) - 96
+
+            graph = Data(x=events[:, 3].view(-1, 1), y=label, pos=events[:, 0:3])
+            grid_sampling = GridSampling(size=[16, 16, 16])
+            grid_sampling(graph)
+            graph.edge_index = radius_graph(graph.pos, 9)
+
+            if self.pre_transform is not None:
+                graph = self.pre_transform(graph)
+
+            torch.save(graph, os.path.join(self.processed_dir, raw_path.split("/")[-1].replace("mat", "pt")))
+
+    def len(self):
+        return len(self.processed_file_names)
+
+    def get(self, idx):
+        data = torch.load(os.path.join(self.processed_dir, self.processed_file_names[idx]))
+        return data
