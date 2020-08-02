667f7209fbe2d416523237c7efe1bfcf63c96d0b
Kai Chen <14sdck@tongji.edu.cn>
2020-08-02 23:26:50 +0800

    - modify the setup.py file

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 import setuptools
 
 
-setuptools.setup(
-    name="tjevents",
-    version="0.1",
-    author="Kai Chen",
-    author_email="14sdck@tongji.edu.cn",
-    description="An event camera data processing library from Institute of Intelligent Vehicle at Tongji University",
-    packages=setuptools.find_packages(exclude=('config', 'tools')),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+
+if __name__ == '__main__':
+
+    setuptools.setup(
+        name="tjevents",
+        version="0.1",
+        author="Kai Chen",
+        author_email="14sdck@tongji.edu.cn",
+        description="A general deep learning based event camera data processing toolbox "
+                    "from Institute of Intelligent Vehicle at Tongji University",
+        long_description=readme(),
+        packages=setuptools.find_packages(exclude=('config', 'tools')),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+    )
